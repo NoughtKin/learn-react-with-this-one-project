dfc28751c528febaded2002f86cb56d0fc22b481
NoughtKin <121004366+NoughtKin@users.noreply.github.com>
2023-05-06 18:42:23 +0700

    Create a function that handles the submit form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [...currentTodos, { id: 123, check: true, title: newItem }];
+    });
+  };
+
   return (
     <div className="new-item-form">
-      <form className="new-item-row">
+      <form onSubmit={handleSubmit} className="new-item-row">
         <label htmlFor="item">New item</label>
         <br></br>
-        <input type="text" id="item"></input>
+        <input
+          value={newItem}
+          onChange={(event) => setNewItem(event.target.value)}
+          type="text"
+          id="item"
+        ></input>
         <button className="btn">Add</button>
       </form>
     </div>
