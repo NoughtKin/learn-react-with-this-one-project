ed099ff6fa0826a1676cd3906db1dab30af987a4
NoughtKin <121004366+NoughtKin@users.noreply.github.com>
2023-05-06 15:53:18 +0700

    Why did he add type=text in `<input>` on line 13?
    Also update README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="new-item-form">
+      <form className="new-item-row">
+        <label htmlFor="item">New item</label>
+        <br></br>
+        <input id="item"></input>
+        <button className="btn">Add</button>
+      </form>
     </div>
   );
 }
