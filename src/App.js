74ebedb45427dfe54d79b84ecabbf9178bb01958
NoughtKin <121004366+NoughtKin@users.noreply.github.com>
2023-05-09 10:25:42 +0700

    I know that I'm currently declare handleSubmit function wrong, but I don't know how to solve it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 export default function App() {
+  const [item, nextItem] = useState("")
+
+  function handleSubmit((event) => {event.target.value})
+
   return (
     <div>
-      <form className="new-item-form">
+      <form className="new-item-form" onSubmit={handleSubmit}>
         <label htmlFor="item">New item</label>
         <br></br>
-        <input type="text" id="item"></input>
+        <input type="text" id="item" ></input>
         <button className="btn">Add</button>
       </form>
       <h1>Todo list</h1>
