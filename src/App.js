19c413a9b74143f08f68973844de9587e2dd4427
NoughtKin <121004366+NoughtKin@users.noreply.github.com>
2023-05-10 18:12:35 +0700

    Add useEffect
    From the tutorial project's source code. I felt like this falls under the 20/80 rule so just copy and use it, for now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NewItemForm } from "./NewItemForm";
 import { TodoList } from "./TodoList";
 
 export default function App() {
-  const [todo, setTodo] = useState([]);
+  const [todo, setTodo] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todo));
+  }, [todo]);
 
   function addTodo(title) {
     setTodo((currentTodo) => {
